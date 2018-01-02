a13b9ae1418a2a82c95989cd6a0584b6571f4785
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-02 19:45:15 +0400

    add fixed html

--- index.js
+++ index.js
@@ -2,30 +2,15 @@ var http = require('http');
 
 var server = http.createServer();
 server.on('request', function (request, response) {
-  response.setHeader("Content-Type", "text/html; charset=utf-8");
-
-  if (request.method === 'GET' && request.url === '/hello') {
-      response.write('<h1>Hello World!</h1>');
-          response.end();
-  } else {
-          response.statusCode = 404;
-          response.write('<h1>404: Zła ścieżka!</h1>');
-          response.end();
-  }
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+        response.write('<h1>Hello World!</h1>');
+            response.end();
+    } else {
+            response.statusCode = 404;
+            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.end();
+    }
 });
 
-
-var WritableFileStream = require('writable-file-stream');
-
-var f = WritableFileStream('./test.txt', {
-    bufferSize: 1000
-});
-
-f.write('test');
-f.write('test');
-f.reopen(); // <- writes here
-f.write('test');
-f.write('test');
-f.end(); // <- writes here
-
-server.listen(8080);
+server.listen(9000);
