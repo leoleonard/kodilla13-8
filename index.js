759a762f8b735c49e80f849a92ce7b1ac202f427
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-02 20:07:11 +0400

    add 404.jpg

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer();
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
-    if (request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-            response.end();
-    } else {
+  //  response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/') {
+      fs.readFile('./index.html', 'utf-8', function (err, data) {
+          response.setHeader("Content-Type", "text/html; charset=utf-8");
+          if (err) throw err;
+          response.write(data);
+          response.end();
+  });
+          } else {
             response.statusCode = 404;
-            response.write('<h1>404: Zła ścieżka!</h1>');
-            response.end();
+        fs.readFile('./404.jpg', function (err, data) {
+          response.setHeader("Content-Type", "image/jpeg");
+          response.write(data);
+          response.end();
+        });
     }
 });
 
