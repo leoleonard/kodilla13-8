279af72f6663ccc45edd23a5b821cee16c70658b
Damian Juzyk <damian.juzyk@gmail.com>
2018-01-04 06:59:34 +0400

    add poprawiony index.js

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
 var http = require('http');
 var fs = require('fs');
+var mime = require('mime-types');
+
+function getFilename(url) {
+   if (url)
+   {
+      var m = url.toString().match(/.*\/(.+?)\./);
+      if (m && m.length > 1)
+      {
+         return m[1] + '.' + url.split('.').pop();
+      }
+   }
+   return "";
+}
+
+function getFileByName(name, response) {
+  fs.readFile(name, function (err, data) {
+      response.setHeader("Content-Type", mime.lookup(name));
+      if (err) throw err;
+      response.write(data);
+      response.end();
+  });
+}
+
+
 
 var server = http.createServer();
 server.on('request', function (request, response) {
-  //  response.setHeader("Content-Type", "text/html; charset=utf-8");
+
     if (request.method === 'GET' && request.url === '/') {
-      fs.readFile('./index.html', 'utf-8', function (err, data) {
-          response.setHeader("Content-Type", "text/html; charset=utf-8");
-          if (err) throw err;
-          response.write(data);
-          response.end();
-  });
-          } else {
-            response.statusCode = 404;
-        fs.readFile('./404.jpg', function (err, data) {
-          response.setHeader("Content-Type", "image/jpeg");
-          response.write(data);
-          response.end();
-        });
+      getFileByName('./index.html', response);
+    } else {
+      var fileName = getFilename(request.url)
+
+      fs.exists('files/' + fileName, function (exists) {
+        if (exists) {
+          getFileByName('files/' + fileName, response);
+        } else {
+          response.statusCode = 404;
+          fs.readFile('./404.jpg', function (err, data) {
+            response.setHeader("Content-Type", "image/jpeg");
+            response.write(data);
+            response.end();
+          });
+        }
+      });
     }
 });
 
